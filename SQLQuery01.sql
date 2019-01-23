51509fa04701f10ffbd40450600d31d06f1850cf
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-23 16:11:37 +0100

    sql server part 01

---
+++ SQLQuery01.sql
@@ -0,0 +1,19 @@
+/*SQL - Création et suppression de bases de données
+ssms
+Penser à actualiser !
+
+CREATE DATABASE nom_de_la_db;
+
+DROP DATABASE nom_de_la_db;
+
+On peut enchainer les lignes de codes en une seule requete : 
+ex : 
+DROP DATABASE students;
+DROP DATABASE webDevelopment;
+
+Ou
+
+DROP DATABASE languages, students
+GO
+
+-- "GO" = ";" */
